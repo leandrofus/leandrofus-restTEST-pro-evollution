f91078c7d4f3759702ab4fc9a9c6707a43110570
Leandro Fusco <leandrofusdev@gmail.com>
2024-01-17 15:32:29 -0300

    ok

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs')
+
+/* GET home page. */
+router.all('/', function(req, res, next) {
+  console.log(req.headers)
+  console.log(req.body )
+  fs.appendFileSync('data/data.log',`${JSON.stringify(req.headers)},  \n ${JSON.stringify(req.body)} \r\n` , )
+  res.sendStatus(200)
+});
+
+module.exports = router;
